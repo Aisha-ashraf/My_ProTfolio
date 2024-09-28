4d8b6f5a20554e684c800b6edb97893a6a3cc945
Aisha-ashraf <143328433+Aisha-ashraf@users.noreply.github.com>
2024-09-28 13:20:02 +0530

    First Commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import { useState } from 'react'
 import logo from '../assets/logo.png';
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     const [nav , setNav] = useState(false);
@@ -19,11 +20,19 @@ const Navbar = () => {
         </div>
       {/*Menu*/}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to = "home" smooth = {true} duration = {500}>Home</Link>
+                    </li>
+                <li> <Link to = "about" smooth = {true} duration = {500}>About</Link>
+                </li>
+                <li> <Link to = "skills" smooth = {true} duration = {500}>Skills</Link>
+                </li>
+                <li> <Link to = "work" smooth = {true} duration = {500}>Work</Link>
+                </li>
+                <li> <Link to = "contact" smooth = {true} duration = {500}>Contact</Link>
+                </li>
+             
+              
             </ul>
        
         {/*Hamburger*/}
@@ -32,11 +41,12 @@ const Navbar = () => {
         </div>
         {/*Mobile Menu */}
         <ul className={!nav ? 'hidden': 'absolute top-0 left-0  w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}> 
-        <li>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Work</li>
-                <li className='py-6 text-4xl'>Contact</li>
+        <li><Link onClick={handleClick} to = "home" smooth = {true} duration = {500}>Home</Link></li>
+                <li className='py-6 text-4xl'><Link  onClick={handleClick} to = "about" smooth = {true} duration = {500}>About</Link>
+                </li>
+                <li className='py-6 text-4xl'><Link onClick={handleClick} to = "skills" smooth = {true} duration = {500}>Skills</Link></li>
+                <li className='py-6 text-4xl'><Link onClick={handleClick} to = "work" smooth = {true} duration = {500}>Work</Link></li>
+                <li className='py-6 text-4xl'><Link  onClick={handleClick} to = "contact" smooth = {true} duration = {500}>Contact</Link></li>
 
         </ul>
         {/*Social ICons*/}
